8b0f018799782df810bd0a4ec520af9439a59496
fujie <fujie@focalcrest.com>
2016-09-01 17:30:23 +0800

    first

---
+++ chat/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.db import transaction
+from django.shortcuts import render, redirect
+import haikunator
+from models import Room
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def new_room(request):
+    """
+    Randomly create a new room, and redirect to it.
+    """
+    new_room = None
+    while not new_room:
+        with transaction.atomic():
+            label = haikunator.haikunate()
+            if Room.objects.filter(label=label).exists():
+                continue
+            new_room = Room.objects.create(label=label)
+    return redirect(chat_room, label=label)
+
+
+def chat_room(request, label):
+    # If the room with the given label doesn't exist, automatically create it
+    # upon first visit (a la etherpad).
+    room, created = Room.objects.get_or_create(label=label)
+
+    # We want to show the last 50 messages, ordered most-recent-last
+    messages = reversed(room.messages.order_by('-timestamp')[:50])
+
+    return render(request, "chat/room.html", {
+        'room': room,
+        'messages': messages,
+    })
