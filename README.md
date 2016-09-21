056d7e6a8f0f397baddbef3ade2f08fa6e287dc1
fujie <fujie@focalcrest.com>
2016-09-21 15:40:28 +0800

    KeysContainer done

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 
 A Django channels sample prj.
 =============================
+
+
+daphne chat.asgi:channel_layer --port 8888
+
+python manage.py runworker
+
+
+from helper.timelife_container import *
+iii = KeysContainer()
+iii.add('1')
+iii.add('2')
+iii.add('3')
+iii.items()
--- helper/timelife_container.py
+++ helper/timelife_container.py
@@ -1,24 +1,42 @@
 # -*- coding: utf-8 -*-
 from django.core.cache import cache
 from collections import deque
+from uuid import uuid4
 
-LIFETIME = 5    
-MAX = 10
+CACHE_KEY = 'k_que'
+LIFETIME = 40
+MAX = 5
 
 
-class TimelifeContainer(object):
+class KeysContainer(object):
+    """
+    实现一个FIFO容器，其中的item保存于cache并带有lifetime.
+    """
 
-    k_list = ['msg_%d' % i for i in range(1, MAX+1)]
-    
-    def __init__(self, cache=cache):
-        self.k_q = deque(
-            [k if cache.get(k) for k in TimelifeContainer.k_list ], 
-            maxlen=MAX
-            )
+    def get_k_que_cache(self):
+        return cache.get_or_set(
+            CACHE_KEY, deque(list(), maxlen=MAX), LIFETIME*30)
 
     def items(self):
-        return
+        v_list = list()
+        k_que = self.get_k_que_cache()
+        if k_que:
+            for key in k_que:
+                v = cache.get(key)
+                if v:
+                    v_list.insert(0, v)
+        return v_list
 
-    def add(self, item):
-        self.q.append()
+    def add(self, *items):
+        for item in items:
+            new_k = uuid4()
+            cache.set(new_k, item, LIFETIME)
+            k_que = self.get_k_que_cache()
+            k_que.append(new_k)
+            cache.set(CACHE_KEY, k_que, LIFETIME*30)
+            print('='*20 + '\n')
+            print('adding item %s in cache %s' % (item, new_k))
+            print('='*20 + '\n')
 
+    def clear(self):
+        cache.delete(CACHE_KEY)
